8e3fe05600d97612f19a0b7430368e9e553a4c85
Benjamin Belaud <benjamin.belaud@hotmail.com>
2025-07-09 15:55:18 +0200

    Set data exemple

--- terraform/04-roleassignment.tf
+++ terraform/04-roleassignment.tf
@@ -1,5 +1,5 @@
 resource "openstack_identity_role_assignment_v3" "ipi-user-1-as" {
   user_id    = openstack_identity_user_v3.ipi-user-1.id
   project_id = openstack_identity_project_v3.ipi.id
-  role_id    = openstack_identity_role_v3.reader.id
+  role_id    = data.openstack_identity_role_v3.reader.id
 }
