87af1b0dbaf406299e2d6ddbb825b9915d74a362
Benjamin Belaud <benjamin.belaud@hotmail.com>
2025-07-09 15:40:22 +0200

    add user

---
+++ terraform/01-user.tf
@@ -0,0 +1,23 @@
+resource "openstack_identity_user_v3" "ipi-user-1" {
+  default_project_id = openstack_identity_project_v3.ipi.id
+  name               = "user-01"
+  description        = "A user"
+
+  password = "password123"
+
+  ignore_change_password_upon_first_use = true
+
+  multi_factor_auth_enabled = true
+
+  multi_factor_auth_rule {
+    rule = ["password", "totp"]
+  }
+
+  multi_factor_auth_rule {
+    rule = ["password"]
+  }
+
+  extra = {
+    email = "ipi@ipi.com"
+  }
+}
